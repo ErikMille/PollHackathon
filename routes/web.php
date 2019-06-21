9e54869f37e2265f55d067d498a53355ed9d3dd0
Adela Spirkova <ad.spirkova@gmail.com>
2019-06-21 10:40:10 +0200

    Route

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/home', 'HomeController@index')->name('home');
+
+Route::resource('/poll', 'PollController');
+
